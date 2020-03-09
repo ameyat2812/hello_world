97fb0f921c526fd618bf3a346b96eea91b8d205b
Ameya Tamebekar <31833755+ameyat2812@users.noreply.github.com>
2020-03-09 14:28:35 +1100

    fix: memory leak

--- Spreadsheet.cc
+++ Spreadsheet.cc
@@ -1,124 +1,140 @@
 // A simple program that computes the square root of a number
-#include <math.h>
 #include <stdio.h>
+#include <string>
 #include <stdlib.h>
 #include <iostream>
 #include <iomanip>
 #include <vector>
 
 using namespace std;
-class Value;
-class Text;
-class Empty;
+
 
 class Glyph {
- public:
-  virtual void Print() const = 0;
+public:
+	virtual void Print() const = 0;
+	virtual ~Glyph() {};
 };
 
 class Empty : public Glyph {
-  public:
-   virtual void Print() const {
-   }
+public:
+	virtual void Print() const {
+	}
 };
 
 class Text : public Glyph {
- public:
-  Text(const string &text) : text_(text) {}
+public:
+	Text(const string &text) : text_(text) {}
+	virtual ~Text() {}
+	virtual void Print() const { cout << text_; }
 
-  void Print() const { cout << text_; }
-
- private:
-  string text_;
+private:
+	string text_;
 };
 
 class Value : public Glyph {
- public:
-  Value(const int &value) : value_(value) {}
+public:
+	Value(const int &value) : value_(value) {}
+	virtual ~Value() {}
+	virtual void Print() const { cout << value_; }
 
-  void Print() const { cout << value_; }
-
- private:
-  int value_;
+private:
+	int value_;
 };
 
 class Spreadsheet : public Glyph {
-  
-  void InsertAfterEmptyElem(vector<Glyph*> &vec, unsigned int num, Glyph *data) {
-    vec.insert(vec.end(), num, new Empty());
-    vec.insert(vec.end(), data);
-  }
-  void InsertAfterEmptyVector(vector<vector<Glyph*> > &vec_vec, unsigned int num, unsigned int col, Glyph *data) {
-    vec_vec.insert(vec_vec.end(), num, vector<Glyph*>());
-    vector<Glyph*> temp(col);
-    temp[col-1] = data;
-    vec_vec.insert(vec_vec.end(), temp);
-    
-  }
- public:
-  Spreadsheet(const unsigned int rows, const unsigned int cols)
-      : sheet_(rows, vector<Glyph*> (cols)){
-  }
 
-  void Update(const unsigned int row, const unsigned int col, Glyph *data) {
-    if(!row || !col)
-      return;
+	void InsertAfterEmptyElem(vector<Glyph*> &vec, unsigned int num, Glyph *data) {
+		vec.insert(vec.end(), num, new Empty());
+		vec.insert(vec.end(), data);
+	}
+	void InsertAfterEmptyVector(vector<vector<Glyph*> > &vec_vec, unsigned int num, unsigned int col, Glyph *data) {
+		vec_vec.insert(vec_vec.end(), num, vector<Glyph*>());
+		vector<Glyph*> temp(col);
+		temp[col - 1] = data;
+		vec_vec.insert(vec_vec.end(), temp);
 
-    bool row_found = false;
-    unsigned int row_number=0;
-    for(auto &&elem:sheet_) {
-      row_number++;
-      if (row_number == row) {
-        row_found = true;
-        break;
-      }
-    }
-    if(row_found) {
-      unsigned int col_number=0;
-      for(auto &&elem:sheet_[row-1]) {
-          col_number++;
-          if(col_number == col) {
-            elem = data;
-            return;
-          }
-      }
-      InsertAfterEmptyElem(sheet_[row-1], (col-col_number-1), data);
-      return;
-    } else {
-      InsertAfterEmptyVector(sheet_, (row-row_number-1), col, data);
-      return;
-    }
-    
-  }
+	}
+public:
+	Spreadsheet(const unsigned int rows, const unsigned int cols)
+		: sheet_(rows, vector<Glyph*>(cols)) {
+	}
 
-  void Print(void) const {
-    for (const auto &row:sheet_) {
-      cout << "|";
-      for (const auto &col:row) {
-        if(col) {
-          cout.width(10);
-          col->Print();
-        } else {
-          cout.width (11);
-        }
-        cout << '|';  
-      }
-      cout << endl;
-    }
-  }
-  private:
-    vector<vector<Glyph*> > sheet_;
+	void Update(const unsigned int row, const unsigned int col, Glyph *data) {
+		if (!row || !col)
+			return;
+
+		bool row_found = false;
+		unsigned int row_number = 0;
+		for (auto &elem : sheet_) {
+			row_number++;
+			if (row_number == row) {
+				row_found = true;
+				break;
+			}
+		}
+		if (row_found) {
+			unsigned int col_number = 0;
+			for (auto &elem : sheet_[row - 1]) {
+				col_number++;
+				if (col_number == col) {					
+					delete(elem);
+					elem = data;
+					return;
+				}
+			}
+			InsertAfterEmptyElem(sheet_[row - 1], (col - col_number - 1), data);
+			return;
+		}
+		else {
+			InsertAfterEmptyVector(sheet_, (row - row_number - 1), col, data);
+			return;
+		}
+
+	}
+
+	void Print(void) const {
+		for (const auto &row : sheet_) {
+			cout << "|";
+			for (const auto &col : row) {
+				if (col) {
+					cout.width(10);
+					col->Print();
+				}
+				else {
+					cout.width(11);
+				}
+				cout << '|';
+			}
+			cout << endl;
+		}
+	}
+
+	void Clear(void) {
+		for (auto &row : sheet_) {
+			for (auto &col : row) {
+				delete(col);
+			}
+			row.clear();
+		}
+		sheet_.clear();
+	}
+private:
+	vector<vector<Glyph*> > sheet_;
 };
 
 int main(int argc, char *argv[]) {
-  auto spread_sheet = Spreadsheet(4, 3);
+	auto spread_sheet = Spreadsheet(4, 3);
+	spread_sheet.Update(1, 1, new Text("bob"));
+	spread_sheet.Update(1, 2, new Value(10));
+	spread_sheet.Update(2, 3, new Text("foo"));
+	spread_sheet.Update(2, 1, new Text("alice"));
+	spread_sheet.Update(4, 4, new Value(5));
+	spread_sheet.Update(5, 7, new Text("total"));
+	
 
-  spread_sheet.Update(1, 1, new Text("bob"));
-  spread_sheet.Update(1, 2, new Value(10));
-  spread_sheet.Update(2, 3, new Text("foo"));
-  spread_sheet.Update(2, 1, new Text("alice"));
-  spread_sheet.Update(4, 4, new Value(5));
-  spread_sheet.Update(6, 7, new Value(46));
+	spread_sheet.Print();
+	
+	spread_sheet.Clear();
+	system("pause");
 
-  spread_sheet.Print();
 }
